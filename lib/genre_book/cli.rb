66b66dc634a99af3da304c6a40ef35ac394c26c6
epic0448 <samuelogundiran@Yahoo.com>
2018-04-30 12:41:50 -0400

    looked in

--- lib/genre_book/cli.rb
+++ lib/genre_book/cli.rb
@@ -2,24 +2,59 @@
 class GenreBook::CLI
 
     def call
-        list of genre
+        puts "Welocme, Type genre to see list of all genres"
+        puts "OR"
+        puts " type in books to see list of books"
+        puts "OR"
+        puts "exit"
+
+        input = gets.strip.downcase
+        
+        if input == "list"
+              list_genre
             #select genre number
-        book_info
+        elsif input == "books"
+              list_book
+        elsif input == "Exit"
+            goodbye
+        else
+            call
+        end
     end 
 
 
-    def list_genre
-        puts "list genere "
+    def list_genre  
+        puts "romaces "
     end
   
 
 
-    def book_info
-        puts "book info"
-        puts  <<-DOC 
-        book 1
-        book 2
-        DOC
+    def list_book
+        puts "Enter the number of book you want to see OR exit ."
+        input = gets.strip.downcase
+        
+        if input ==  "1"
+            puts  <<-DOC 
+             book 1   
+             DOC
+
+        elsif input ==  "2"
+            puts "book 2"
+        elsif input == "list"
+            list_genre
+        elsif input == "exit"
+            goodbye
+        else
+            list_book
+        end
 
     end
+
+
+    def goodbye
+      puts "always a pleasure with you"
+    end
+
+
+   
 end
